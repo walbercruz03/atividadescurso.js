e58d1b8faf2c75185f20101161d5458973c5393b
WALBER VICTOR CRUZ DE LIMA <a95827678@rn.senac.local>
2025-09-25 21:03:50 -0300

    25

---
+++ composição/metodos.js
@@ -0,0 +1,32 @@
+import PromptSync from "prompt-sync";
+const prompt = PromptSync()
+
+
+export class Memoria {
+    #tipo
+    #frequencia
+    #capacidade
+    constructor(tipo,frequencia,capacidade){
+        this.#tipo = tipo
+        this.#frequencia = frequencia
+        this.#capacidade = capacidade
+    } get capacidade(){
+        return this.#capacidade
+    }set capacidade(novamem){
+        this.#capacidade = novamem
+    }
+
+    metusarmemoria(){
+        let qtd = Number( prompt ('Informe a quantidade que deseja usar: '))
+
+        if (qtd<this.#capacidade){
+            this.#capacidade -= qtd
+
+            console.log (`Você usou ${qtd} resta ${this.#capacidade}`)
+        }
+    }
+
+} 
+
+let novam = new Memoria ('ram','1.5', 1024)
+novam.metusarmemoria()
